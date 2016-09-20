4e9d2e3066eae3f31e127805e2283de263efe600
Raphaël D <raphael.dujardin@ensea.fr>
2016-09-20 15:41:13 +0200

    Initialization

---
+++ build/CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run.dir/src/main.cpp.o"
+  "CMakeFiles/run.dir/src/state/Exemple.cpp.o"
+  "../bin/run.pdb"
+  "../bin/run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
